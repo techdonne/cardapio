a8b5fde045180d63d39907256436242068907d16
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-07-28 12:57:39 -0300

    Web config

--- src/main/java/com/donnetech/cardapio/config/WebConfig.java
+++ src/main/java/com/donnetech/cardapio/config/WebConfig.java
@@ -1,2 +1,14 @@
-package com.donnetech.cardapio.config;public class WebConfig {
+package com.donnetech.cardapio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
 }
